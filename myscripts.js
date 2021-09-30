95bbcce97e9de07549b43dfcaa34eb012c9631ef
mannotserious <67032508+mannotserious@users.noreply.github.com>
2021-09-30 13:40:13 +1300

    Added files

---
+++ myscripts.js
@@ -0,0 +1,28 @@
+
+// code retrieved from: https://stackoverflow.com/questions/18336896/adding-a-dollar-sign-to-a-text-field-input-using-javascript.// Javascript comment ability retrieved from: https://www.w3schools.com/js/js_comments.asp.
+
+
+//function for register button change
+function changehtml(){
+	document.getElementById("registerbutton").innerHTML = "You're all done! <br> Registration Successful!";
+}
+
+function validateForm() {
+	var x = document.value; {
+
+}
+
+//remain on page
+e.preventDefault();
+
+}
+
+function performOp(){
+	var x = document.forms["servicech"]["number"].value;
+	var changedX = x * 50; {
+	alert("$"+changedX+" - (register button)");
+}
+
+//remain on page
+e.preventDefault();
+}
